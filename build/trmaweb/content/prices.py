38bf3ba9ddae40780a8569feccba2982e87194dd
Kendall Bailey <kendall@trma.us>
2018-09-10 21:35:11 -0500

    Start using a base layout.  Fix anchor links.

--- build/trmaweb/content/prices.py
+++ build/trmaweb/content/prices.py
@@ -6,8 +6,12 @@ def fill_price(snip,lbl,value) :
     snip = re.sub(r'__family__',lbl,snip)
     return snip
 
+def make_anchor(id) :
+	return '<span class="trma-inst-anchor" id="%s"></span>' % id
+
 context = {
 
- '*' : { 'family_price' : fill_price }
+ '*' : { 'family_price' : fill_price, 
+         'make_anchor' : make_anchor }
 
 }
