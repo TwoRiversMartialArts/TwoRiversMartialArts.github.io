6c84d84843fe480577aa3df9666556068571561c
Kendall Bailey <kendall@trma.us>
2018-09-16 15:56:00 -0500

    First deployment as primary web site

--- build/trmaweb/content/linkto.py
+++ build/trmaweb/content/linkto.py
@@ -1,15 +1,23 @@
 
-
 def linktofn( prefix ) :
-	def lt(x) :
-		return '"%s%s"' % (prefix,x)
-	return lt
+    def lt(x) :
+        if 'index.html' in x :
+            return '"../index.html"'
+        return '"%s%s"' % (prefix,x)
+    return lt
+
+def indexlt(x) :
+    # if x == '#' : return '"index.html#"'
+    if 'index.html' in x : 
+        return x
+    return linktofn('web/')(x)
 
 context = {
 
   '*' :          { 'linkto' : linktofn('') },
   # if index is outside folder of other pages, 
   #   set the prefix accordingly
-  'index.html' : { 'linkto' : linktofn('') }  
+  
+  'index.html' : { 'linkto' : indexlt }  
   
 }
