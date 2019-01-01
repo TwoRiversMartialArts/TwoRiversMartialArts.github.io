2ac71c877481d20b6a7197afafc3c453f60815e7
Kendall Bailey <kendall@baileyplex.com>
2019-01-01 10:39:20 -0600

    Add Mr. Miles memoriam

--- build/trmaweb/content/memoriams.py
+++ build/trmaweb/content/memoriams.py
@@ -11,12 +11,23 @@ context = {
           <p>
           Master Samuelson began training with Master Heintz in the early 1980s and earned
           his 6th Dan in April 2008. He was the primary instructor for the Indianola
-          Branch. He is a former President, and former Vice-President of the TRMA Board
+          Branch. He was a former President, and former Vice-President of the TRMA Board
           of Directors.
           </p>
           <div style="clear:both"></div>
         ''',
         '''<a href="https://www.legacy.com/obituaries/desmoinesregister/obituary.aspx?n=marvin-samuelson&pid=190873832&fhid=17489">
            November 29th, 2018
-        ''')
+           </a>
+        '''),
+        (
+        '''
+        <p>Mr. Richard Miles</p>
+        '''
+        ,
+        '''<a href="https://www.legacy.com/obituaries/desmoinesregister/obituary.aspx?n=richard-j-miles&pid=174263675">
+            2015
+           </a>
+        '''
+        )
 ]}}
